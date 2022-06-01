ddc95b063f1bf2544e460c74504d309631ca4496
U04598637 <u04598637@tuks.co.za>
2022-06-02 00:01:21 +0200

    Home & about page + working on file upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace u04598637_HW03.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(HttpPostedFileBase files, string radioresult)
+        {
+            var result = Request.Form["radioresult"];
+            
+
+                var fileName = Path.GetFileName(files.FileName);
+
+
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+
+
+                files.SaveAs(path);
+
+            return null;
+
         }
 
         public ActionResult Files()
---
+++ Models/FileModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace u04598637_HW03.Models
+{
+    public class FileModel 
+    {
+        [Display(Name = "File Name")]
+
+        public string FileName { get; set; }
+    }
+}
