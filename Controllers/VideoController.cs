f8e12afda26d72d6a053c57085645e08de9ce252
U04598637 <u04598637@tuks.co.za>
2022-06-05 00:27:17 +0200

    Video file and controller

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using u04598637_HW03.Models;
+using System.IO;
+
+namespace u04598637_HW03.Controllers
+{
+    public class VideoController : Controller
+    {
+        // GET: Video
+        [HttpGet]
+        public ActionResult Index()
+        {
+            string[] filePaths = Directory.GetFiles(Server.MapPath("~/Media/Videos/"));
+            List<FileModel> files = new List<FileModel>();
+
+            foreach (string filePath in filePaths)
+            {
+                files.Add(new FileModel { FileName = Path.GetFileName(filePath) });
+                ViewBag.videos = filePaths;
+            }
+            return View(files);
+        }
+
+        public FileResult DownloadFile(string fileName)
+        {
+
+            string path = Server.MapPath("~/Media/Videos/") + fileName;
+
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            return File(bytes, "application/octet-stream", fileName);
+        }
+
+        public ActionResult DeleteFile(string fileName)
+        {
+            string path = Server.MapPath("~/Media/Videos/") + fileName;
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+
+            System.IO.File.Delete(path);
+            return RedirectToAction("Index");
+        }
+    }
+}
